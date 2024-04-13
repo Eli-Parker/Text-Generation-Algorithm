b3e853cf719ee8fb8ad9f8b7d699586f7aaa8fdd
Swaey-1 <racecar47@icloud.com>
2024-04-13 13:21:30 -0600

    Initial commit, added main class and javadocs for methods

---
+++ src/GenerativeTextAlgorithm/GenerativeTextAlgorithm.java
@@ -0,0 +1,22 @@
+package GenerativeTextAlgorithm;
+
+/**
+ * Contains a generative text algorithm which generates predicted text based on a given input
+ * <P>
+ * Implements compression on the given text and uses a weighted directed graph to determine most
+ * probable next word given a prompt.
+ * @version Apr 13, 2024
+ * @author Eli Parker and Jorden Dickerson
+ */
+public class GenerativeTextAlgorithm
+{
+    /**
+     * Method where actual code is run.
+     * TODO outline process in this method
+     *
+     * @param args command line arguments to add to program TODO use these to get text docs
+     */
+    public static void main(String[] args) {
+
+    }
+}
