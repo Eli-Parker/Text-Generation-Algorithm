15b234a40c42e5101ca0d05683760eacf6b27f15
Swaey-1 <racecar47@icloud.com>
2024-04-13 14:06:05 -0600

    Added test files for finished alg, outlined process with TODO's in TextGenerator.java

--- src/comprehensive/TextGenerator.java
+++ src/comprehensive/TextGenerator.java
@@ -10,13 +10,25 @@ package comprehensive;
  */
 public class TextGenerator
 {
+    //TODO finish readme
+    //TODO another class for processing file
+    //TODO determine data structure to use for this (I vote directed graph of some sort)
     /**
      * Method where actual code is run.
      * TODO outline process in this method
      *
      * @param args command line arguments to add to program TODO use these to get text docs
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        //TODO the process
+        /*
+            1. handle command line arguments.
+                - first, We will have to first take the file as the first argument and process it,
+                  we should likely make a second class in the package dedicated to this process.
+                - second, the seed word. This is what we'll generate our text from
+                - a variable K for the third argument, tells us how many words to predict
+                - handle fourth arg according to assignment spec
+         */
     }
 }
